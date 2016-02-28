c5e3582cd3a7d21c2cb04b6d7615d7063cb45fac
Julian Sanin <sanin89julian@gmail.com>
2016-02-28 16:40:59 +0100

    Updated code to support RFM69HW radios

--- sensor-nodes/sketch-receiver/sketch-receiver.ino
+++ sensor-nodes/sketch-receiver/sketch-receiver.ino
@@ -1,3 +1,27 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2016 Julian Sanin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*******************************************************************************/
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -10,7 +34,8 @@
 #endif
 #define LED_BUILTIN 9 // On Moteino R4 LED is connected to D9 instead of D13.
 
-Rfm69<SpiDevice<10> > rfm69;
+// RFM69HW radio with Slave Select on Arduino pin 10.
+Rfm69<SpiDevice<10>, Rfm69ModelHW> rfm69;
 
 uint8_t rxBuffer[64];
 
--- sensor-nodes/sketch-sensor-simulation/sketch-sensor-simulation.ino
+++ sensor-nodes/sketch-sensor-simulation/sketch-sensor-simulation.ino
@@ -1,3 +1,27 @@
+/*******************************************************************************
+The MIT License (MIT)
+
+Copyright (c) 2016 Julian Sanin
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
+*******************************************************************************/
+
 #include <stddef.h>
 #include <stdint.h>
 
@@ -12,8 +36,8 @@
 #endif
 #define LED_BUILTIN 9 // On Moteino R4 LED is connected to D9 instead of D13.
 
-// RFM69 radio with Slave Select on Arduino pin 10.
-Rfm69<SpiDevice<10> > rfm69;
+// RFM69HW radio with Slave Select on Arduino pin 10.
+Rfm69<SpiDevice<10>, Rfm69ModelHW> rfm69;
 
 uint8_t txBuffer[62];
 
@@ -29,7 +53,7 @@ void setup() {
 	// RFM69 radio.
 	rfm69.init(28, 42, 8686);
 	//rfm69.encrypt("mysecret");
-	//rfm69.txPower(15); // 0 = min .. 31 = max
+	//rfm69.setTransmitPower(+17/*dBm*/);
 	rfm69.sleep();
 	for (size_t i = 0; i < sizeof(txBuffer); i++)
 		txBuffer[i] = (uint8_t)i;
