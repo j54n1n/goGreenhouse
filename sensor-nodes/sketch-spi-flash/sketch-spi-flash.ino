7267f3118c464a8736095db42bc13d3edde16422
Julian Sanin <sanin89julian@gmail.com>
2016-03-02 23:10:07 +0100

    Finalized SpiFlash driver

--- sensor-nodes/sketch-spi-flash/sketch-spi-flash.ino
+++ sensor-nodes/sketch-spi-flash/sketch-spi-flash.ino
@@ -1,36 +1,25 @@
 #include <SPI.h>
 #include <SpiDevice.h>
+#include <SpiFlash.h>
 
 // W25X40CL SPI Flash memory.
-SpiDevice<8> spi;
+SpiFlash<SpiDevice<8> > flash;
 
-void printBytes(uint8_t* buf, size_t len) {
-	for (size_t i = 0; i < len; i++) {
-		Serial.print(buf[i] >> 4, HEX);
-		Serial.print(buf[i] & 0xF, HEX);
-	}
-	Serial.println();
-}
+union Id {
+	uint64_t val;
+	struct {
+		uint32_t lo;
+		uint32_t hi;
+	} ints;
+};
 
 void setup() {
 	Serial.begin(115200);
-	spi.master();
-	Serial.println("SPI Flash Unique ID");
-
-	// Setup W25X40CL SPI Flash memory to read unique ID number.
-	uint8_t data[
-		1/*Command Byte*/ + 4/*Dummy Bytes*/ + 8/*Unique ID bytes*/] = { 0 };
-	data[0] = 0x4B;
-
-	printBytes(data, sizeof(data));
-	spi.transferBulk(data, sizeof(data));
-	printBytes(data, sizeof(data));
-
-	uint64_t uniqueId;
-	memcpy(&uniqueId, data + 5, sizeof(uint64_t)); // Reverse order?
-	//uniqueId = *(uint64_t *)(data + 5); // Same as above.
-	Serial.print((uint32_t)(uniqueId >> 32), HEX);
-	Serial.println((uint32_t)(uniqueId >> 0xFFFFFFFF), HEX);
+	flash.init();
+	Id id;
+	id.val = flash.getUniqueId();
+	Serial.print(id.ints.hi, HEX);
+	Serial.print(id.ints.lo, HEX);
 }
 
 void loop() {
