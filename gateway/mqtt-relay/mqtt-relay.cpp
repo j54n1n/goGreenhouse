c2f81c734141992621288ee50920466f8a3fd802
Julian Sanin <sanin89julian@gmail.com>
2016-03-15 13:52:50 +0000

    Added code for RFM69 receive

--- gateway/mqtt-relay/mqtt-relay.cpp
+++ gateway/mqtt-relay/mqtt-relay.cpp
@@ -24,12 +24,26 @@ THE SOFTWARE.
 
 // Demo reception of a topic.
 
+#include <cstdint>
+#include <cstring>
 #include <iostream>
 #include <string>
 
 #include <mosquittopp.h>
 
+#include <SpiDevice/SpiDevice.h>
+#include <Rfm69/Rfm69.h>
+
 using namespace std;
+
+struct Packet {
+	uint64_t uniqueId;
+	uint8_t packetType;
+	uint8_t packetVersion;
+	uint8_t sequenceNumber;
+	int8_t temperature;
+} payload;
+
 
 int topicMessageId = 1; // Message of type string.
 
@@ -60,7 +74,15 @@ struct Rfm69Mqtt : public mosqpp::mosquittopp {
 
 Rfm69Mqtt mqtt;
 
+// RFM69HW radio with Slave Select on CE0.
+Rfm69<SpiDevice<0>> rfm69;
+
+uint8_t rxBuffer[64] = { 0 };
+
 int main(void) {
+	// RFM69 radio.
+        rfm69.init(63, 42, 8686);
+
 	cout << "[Rfm69MqttRelay]" << endl;
 	mqtt.connect("127.0.0.1");
 	// For example: mosquitto_pub -t raw/rfm69/8686/42 -m "My Text"
@@ -68,7 +90,39 @@ int main(void) {
 		0 /*Message Id*/,
 		"raw/rfm69/8686/42" /*topic @rfm69, @868.6MHz, @group=42*/);
 	while (true) {
-		// Keep going.
-		mqtt.loop();
+		// Keep going mosquitto.
+		mqtt.loop(1/*ms*/);
+
+		int length = rfm69.receive(rxBuffer, sizeof(rxBuffer));
+		if (length >= 0) {
+			cout << "OK " << length << "byte(s) ";
+			for (int i = 0; i < length; i++) {
+				if (i == 2) {
+					// Start of payload.
+					cout << " [";
+				}
+				cout << hex << (int)(rxBuffer[i] >> 4);
+				cout << hex << (int)(rxBuffer[i] & 0x0F);
+			}
+			cout << "] (" << dec << (int)rfm69.getRssiValue() << "dBm";
+			cout << ((rfm69.afc < 0) ? "" : "+") << rfm69.afc << ":";
+			cout << (int)rfm69.getLnaGain() << "dB)" << endl;
+
+			// Decoded packet.
+			cout << "Packet:";
+			uint8_t parity = ((rxBuffer[0] & 0xC0) >> 6);
+			uint8_t header = ((rxBuffer[1] & 0xC0) | (rxBuffer[0] & 0x3F));
+			uint8_t sourceId = rxBuffer[1] & 0x3F;
+			cout << " p=" << hex << (int)parity;
+			cout << " h=" << hex << (int)header;
+			cout << " srcId=" << dec << (int)sourceId;
+			memcpy(&payload, &rxBuffer[2], length - 2);
+			cout << " [uniqueId=0x" << hex << payload.uniqueId;
+			cout << " packetType=0x" << hex << (int)payload.packetType;
+			cout << " packetVersion=0x" << hex << (int)payload.packetVersion;
+			cout << " sequenceNumber=0x" << hex << (int)payload.sequenceNumber;
+			cout << " temperature=" << dec << (int)payload.temperature;
+			cout << " degree C]" << endl;
+		}
 	}
 }
